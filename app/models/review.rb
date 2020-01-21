48595196ab12369916e6ee30b32d1c3970c64584
Luiz Romario Filho <luizromariofilho@gmail.com>
2020-01-20 22:45:54 -0300

    Adding validates in Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :reviewable, polymorphic: true
+
+	validates :comment, :reviewable_type, :reviewable_id, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Review, type: :model do
   it { should have_db_column(:reviewable_type).of_type(:string) }
 
   it { should belong_to(:reviewable) }
+
+  context "validations" do
+  	subject { create(:review_on_restaurant) }
+
+  	it "is valid with valid attributes" do
+	    should be_valid
+	 	end	
+
+	 	it "is not valid without a comment" do 
+	    should validate_presence_of(:comment)
+	  end
+
+	  it "is not valid without a reviewable" do 
+	    should validate_presence_of(:reviewable_type)
+	    should validate_presence_of(:reviewable_id)
+	  end
+  end
 end
